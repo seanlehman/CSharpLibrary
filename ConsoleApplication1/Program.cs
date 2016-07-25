825e5d056e7acf52c1bdc6fca06d2f3da08b3b00
seanlehman <sjlehman4@comcast.net>
2016-07-25 15:47:09 -0400

    Proj1 Console Applications

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj1Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "a string";
+            string s2 = "This is a \r\n multi-line string";
+            Console.WriteLine(s);
+            Console.WriteLine();
+            Console.WriteLine(s2);
+
+            //Concatenation
+            string s3 = s + s2;
+            Console.WriteLine($"Concatenation:   {s3}");
+            Console.WriteLine();
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
